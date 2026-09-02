9365d914c455e190c77b06ac4c2ca0558b838fe9
k-everly <cfeverly@ncsu.edu>
2026-09-02 14:31:01 -0400

    Update 0_download_GEO_data.R

--- 2026_Everly_childhood_obesity_ICRs/0_download_GEO_data.R
+++ 2026_Everly_childhood_obesity_ICRs/0_download_GEO_data.R
@@ -1,5 +1,6 @@
-if (!requireNamespace("BiocManager", quietly = TRUE))
+if (!requireNamespace("BiocManager", quietly = TRUE)){
   install.packages("BiocManager")
+}
 BiocManager::install("GEOquery")
 library(GEOquery)
 library(readr)
