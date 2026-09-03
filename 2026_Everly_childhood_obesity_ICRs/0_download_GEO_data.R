e468a21b79913b632b8f0412a8fe170248716655
k-everly <cfeverly@ncsu.edu>
2026-09-03 16:09:19 -0400

    Update 0_download_GEO_data.R
    Added code to correctly download study metadata

--- 2026_Everly_childhood_obesity_ICRs/0_download_GEO_data.R
+++ 2026_Everly_childhood_obesity_ICRs/0_download_GEO_data.R
@@ -5,10 +5,19 @@ BiocManager::install("GEOquery")
 library(GEOquery)
 library(readr)
 
+
 # Create a temporary directory for GEO files
 geo_id <- "GSE334366"
 tmp <- file.path(tempdir(), geo_id)
 dir.create(tmp, showWarnings = FALSE, recursive = TRUE)
+
+# Download Study Metadata
+gset <- getGEO(
+  geo_id,
+  GSEMatrix = TRUE
+)
+
+study_data <- pData(gset[[1]])
 
 # Download GEO supplementary files to temporary directory
 getGEOSuppFiles(geo_id, baseDir = tmp)
@@ -16,10 +25,10 @@ getGEOSuppFiles(geo_id, baseDir = tmp)
 # Check downloaded files
 list.files(tmp, recursive = TRUE)
 
-# Load processed CpG beta-value dataframes and study metadata
+# Load processed CpG beta-value dataframes and raw BMI dat
 processed_cpg_df_umbilical <- read_csv(list.files(tmp, pattern = "umbilical_cord_beta_matrix.csv(\\.gz)?$", recursive = TRUE, full.names = TRUE)[1])
 processed_cpg_df_peripheral <- read_csv(list.files(tmp, pattern = "peripheral_blood_beta_matrix.csv(\\.gz)?$", recursive = TRUE, full.names = TRUE)[1])
-study_data <- read_csv(list.files(tmp, pattern = "\\Supplementary_data_obesity.csv(\\.gz)?$", recursive = TRUE, full.names = TRUE)[1])
+raw_bmi_data <- read_csv(list.files(tmp, pattern = "\\Supplementary_data_obesity.csv(\\.gz)?$", recursive = TRUE, full.names = TRUE)[1])
 
 # Load raw IDATs
 raw_tar <- list.files(tmp, pattern = "GSE334366_RAW\\.tar$", recursive = TRUE, full.names = TRUE)[1]
